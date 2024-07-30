cce25ee1ecbd23e3c49a8d4418bbb275f4bd2a8a
Julian B <bechti44@googlemail.com>
2024-07-30 16:13:59 +0200

    add support for WSL

--- UnixCommandInvokeHelper/ProcessCommandHelper.cs
+++ UnixCommandInvokeHelper/ProcessCommandHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace UnixCommandInvokeHelper
 {
@@ -11,15 +12,23 @@ namespace UnixCommandInvokeHelper
     public class ProcessCommandHelper
     {
         /// <summary>
-        /// executes the command.
+        /// Converts a Windows path to a WSL path.
+        /// </summary>
+        /// <param name="windowsPath">The Windows path to convert</param>
+        /// <returns>The converted WSL path</returns>
+        public string ConvertPathToWsl(string windowsPath)
+        {
+            return "/mnt/" + windowsPath.Substring(0, 1).ToLower() + windowsPath.Substring(2).Replace("\\", "/");
+        }
+        /// <summary>
+        /// Executes the command.
         /// </summary>
         /// <remarks>
-        /// after the task is finished, you can access the results with command.Errors and command.Output
+        /// After the task is finished, you can access the results with command.Errors and command.Output.
         /// </remarks>
-        /// <returns>awaitable Tak</returns>
-        /// <param name="commandText">the command to execute</param>
-        /// <param name="workingDirectory">the directory path to execute the command in (optional)</param>
-        /// <returns></returns>
+        /// <returns>Awaitable Task</returns>
+        /// <param name="commandText">The command to execute</param>
+        /// <param name="workingDirectory">The directory path to execute the command in (optional)</param>
         public async Task<CommandResult> ExecuteAsync(string commandText, DirectoryInfo? workingDirectory = null)
         {
             CommandResult result = new CommandResult();
@@ -33,14 +42,30 @@ namespace UnixCommandInvokeHelper
                     var processStartInfo = new ProcessStartInfo()
                     {
                         WindowStyle = ProcessWindowStyle.Hidden,
-                        FileName = "/bin/bash",
-                        Arguments = $"-c \"{escapedArgs}\"",
                         RedirectStandardOutput = true,
                         RedirectStandardError = true,
                         UseShellExecute = false
                     };
-                    if (workingDirectory != null)
-                        processStartInfo.WorkingDirectory = workingDirectory.FullName;
+
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        processStartInfo.FileName = "wsl.exe";
+                        string commandstring = "";
+                        if (workingDirectory != null) commandstring = $"cd {ConvertPathToWsl(workingDirectory.FullName)} &&";
+                        commandstring += escapedArgs;
+                        processStartInfo.Arguments = $"-e bash -c \"{commandstring}\"";
+                    }
+                    else
+                    {
+                        processStartInfo.FileName = "/bin/bash";
+                        processStartInfo.Arguments = $"-c \"{escapedArgs}\"";
+                        if (workingDirectory != null)
+                        {
+                            processStartInfo.WorkingDirectory = workingDirectory.FullName;
+                        }
+                    }
+                        
+
                     process.StartInfo = processStartInfo;
                     process.Start();
 
@@ -77,10 +102,10 @@ namespace UnixCommandInvokeHelper
         /// Using sudo in scripts and applications can pose significant security risks, especially when passwords are involved.
         /// Only use this method in trusted and secure environments.
         /// </warning>
-        /// <param name="commandText">the command to execute (you do not have to prefix sudo)</param>
-        /// <param name="password">the password to use</param>
-        /// <param name="workingDirectory">the directory to execute the command in</param>
-        /// <returns></returns>
+        /// <param name="commandText">The command to execute (you do not have to prefix sudo)</param>
+        /// <param name="password">The password to use</param>
+        /// <param name="workingDirectory">The directory to execute the command in</param>
+        /// <returns>Awaitable Task</returns>
         public async Task<CommandResult> ExecuteSudoAsync(string commandText, string password = "", DirectoryInfo? workingDirectory = null)
         {
             if (!string.IsNullOrEmpty(password))
---
+++ UnixCommandInvokeHelperTests/ProcessCommandHelperTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+using UnixCommandInvokeHelper;
+
+namespace UnixCommandInvokeHelperTests;
+
+public class ProcessCommandHelperTests
+{
+    [Fact]
+    public async Task ListDirectoryContents_ShouldWorkCrossPlatform()
+    {
+        // Arrange
+        var processCommandHelper = new ProcessCommandHelper();
+        string commandText;
+            
+        // Use a directory that exists on both Windows (via WSL) and Unix systems
+        string directoryPath = "/tmp";
+
+        // Act
+        var result = await processCommandHelper.ExecuteAsync($"ls {directoryPath}");
+
+        // Assert
+        Assert.False(string.IsNullOrEmpty(result.Output), "The command output should not be empty.");
+        Assert.True(result.Errors == string.Empty, $"The command should not produce errors. Errors: {result.Errors}");
+    }
+    [Fact]
+    public async Task ListDirectoryContents_ShouldWorkWindows()
+    {
+        // Arrange
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var processCommandHelper = new ProcessCommandHelper();
+            var result = await processCommandHelper.ExecuteAsync($"ls /mnt/c/temp");
+
+            // Assert
+            Assert.False(string.IsNullOrEmpty(result.Output), "The command output should not be empty.");
+            Assert.True(result.Errors == string.Empty, $"The command should not produce errors. Errors: {result.Errors}");
+        }
+    }
+    [Fact]
+    public async Task ListDirectoryContents_ShouldWorkWithWorkingDirOnWindows()
+    {
+        // Use a directory that exists on both Windows (via WSL) and Unix systems
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var processCommandHelper = new ProcessCommandHelper();
+            DirectoryInfo testdir = new DirectoryInfo(@"C:\temp");
+            var result = await processCommandHelper.ExecuteAsync("ls", testdir);
+
+            // Assert
+            Assert.False(string.IsNullOrEmpty(result.Output), "The command output should not be empty.");
+            Assert.True(result.Errors == string.Empty, $"The command should not produce errors. Errors: {result.Errors}");
+        }
+    }
+}
